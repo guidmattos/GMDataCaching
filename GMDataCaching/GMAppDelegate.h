41e36076fd289ea5752191df376d3f3265be6423
Guilherme Mattos <mattos@incube.mobi>
2014-09-12 11:24:52 -0300

    Initial Commit

---
+++ GMDataCaching/GMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GMAppDelegate.h
+//  GMDataCaching
+//
+//  Created by Guilherme Duarte Mattos on 9/12/14.
+//  Copyright (c) 2014 Guilherme Duarte Mattos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GMDataCaching/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GMDataCaching
+//
+//  Created by Guilherme Duarte Mattos on 9/12/14.
+//  Copyright (c) 2014 Guilherme Duarte Mattos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GMAppDelegate class]));
+    }
+}
